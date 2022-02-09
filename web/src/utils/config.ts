5c3b1860326fc3df605be1111f920cad9909ed48
isveny <amineprojet7@gmail.com>
2022-02-09 16:33:20 +0100

    changed config

--- web/src/utils/config.ts
+++ web/src/utils/config.ts
@@ -3,7 +3,7 @@ export const config = {
     client_id: "1c05bb12dac24c4fa669d4377de2e03a",
     client_secret: "e87ff24e50224779b1030f0fb948ce60",
     refresh_token:
-      "BQCW6rX4qsdd8N8uHMfv-queBJUjCqnyb6zI9APB2R137FI6bUPOTHgxEPi-fEeTTVkrRj3iChpJF9GRCDwgCg2lnlupfAevwJkQx8PH31heT0GlFvTk8Y_PBQHyzUatVRvTyaXUU3cW2hqDj-GYgBw7T7bvyw-zGACetmgBlQk14Jgyf2vu-ivj",
+      "BQA0JOmwEKkDejnFKgyuvTvjUDVWeCH5wBS60n1hVAlDYE-7IT0VDK6oX7LcTJtwVom6ew3U7GmhQ1l5RjoqfDVb13_Nq0YvkarGRagJEbwpxJsY0kXfc66wwdfCaK6a1SRaiDSalnHBXJObB7oMiecPiOoyEqsRRoH1g3Hiq1x8X5jfVYP9XPmufWs",
     token: "https://accounts.spotify.com/api/token",
   },
 };
